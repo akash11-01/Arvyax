3e59ec28d78e77b2138ab915f995dc0e697e17c4
Akash Jaiswar <akashjaiswar227@gmail.com>
2025-08-03 22:14:10 +0530

    Hosted urls added

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,12 @@ const PORT = process.env.PORT || 5000;
 
 await connectDb();
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://wellness-session-management.netlify.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://arvyax-backend-lui0.onrender.com/",
         secure: false,
       },
     },
